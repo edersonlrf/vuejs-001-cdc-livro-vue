3b39e663994e2cefc461177f7add4380eb79f40e
TI Br Supply <ti@brsupply.com.br>
2021-06-02 17:10:31 -0300

    4.2 EXIBINDO DADOS COM DIRETIVAS

--- criando-exibindo-dados/src/App.vue
+++ criando-exibindo-dados/src/App.vue
@@ -1,21 +1,7 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <h1 v-text="titulo"></h1>
+    <h2>{{ subtitulo }}</h2>
   </div>
 </template>
 
@@ -28,6 +14,9 @@ export default {
       subtitulo: 'Defina uma descrição',
       tarefas: []
     }
+  },
+  mounted () {
+    setTimeout( () => this.titulo = "Novo título", 2000 );
   }
 }
 </script>
